c6150452664cad38ea54f80447281cb05de5a81a
Michael Baghel <michael.baghel@gmail.com>
2021-03-04 18:49:09 +0400

    basic serverless config

---
+++ dlpFunctions/create.ts
@@ -0,0 +1,17 @@
+export const create = (event, context) => {
+ 
+  console.log(event);
+  console.log(context);
+
+  const res = {
+    statusCode: 200,
+    body: JSON.stringify({ 
+      success: true,
+      data: {
+        message: "OK",
+      }
+    })
+  }
+
+  return res
+}
